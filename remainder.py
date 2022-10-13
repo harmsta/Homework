c29dc6275b99bc667b2ccf4da80da8c67f946b4a
harmsta <113304271+harmsta@users.noreply.github.com>
2022-10-13 13:02:44 -0500

    Add files via upload

---
+++ remainder.py
@@ -0,0 +1,6 @@
+dividend = int(input('What is the dividend'))
+divisor = int(input('What is the divisor'))
+quotient = dividend // divisor
+remainder = dividend % divisor
+
+print(f'Quotient:{quotient}  Remainder:{remainder}')
